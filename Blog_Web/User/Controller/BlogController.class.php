bd277f900549da220ae776242236525229e2555a
Jiar <jiar.world@gmail.com>
2016-10-15 15:52:27 +0800

    修改用户端Controller，开始优化管理员界面。

---
+++ Blog_Web/User/Controller/BlogController.class.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Created by: Jiar
+ * Github: https://www.github.com/Jiar/
+ * Date: 2016/10/14
+ * Time: 上午11:59
+ */
+
+namespace User\Controller;
+
+use Think\Controller;
+
+
+class BlogController extends Controller {
+
+    public function addBlogAction() {
+        if(session('?userId') && session('?userToken')) {
+            $this->display('Blog/addBlog');
+        } else {
+            redirect('/User/User/user');
+        }
+    }
+
+    public function addBlogFormAction() {
+        $blog = D('Blog');
+        $data['user_id'] = session('userId');
+        $blog = $blog->where($data)->order('blog_id desc')->select();
+        $blog_id = $blog[0]['blog_id']+1;
+        $blog = D('Blog');
+        $data['title'] = I('post.title');
+        $data['user_id'] = session('userId');
+        $data['blog_id'] = $blog_id;
+        $data['content'] = I('post.content');
+        $data['create_time'] = date('Y-m-d H:i:s');
+        $data['last_modify_time'] = date('Y-m-d H:i:s');
+        if(!$blog->create($data)) {
+            $this->error(structureErrorInfo($blog->getError()));
+        } else {
+            $blog->add();
+            redirect('/User/User/user');
+        }
+    }
+
+    public function modifyBlogAction($userId, $blogId) {
+        if(session('?userId') != $userId) {
+            $this->error('无法修改别人的博客');
+            return;
+        }
+        if(session('?userId') && session('?userToken')) {
+            $blog = D('Blog');
+            $data['user_id'] = $userId;
+            $data['blog_id'] = $blogId;
+            $blog = $blog->where($data)->select();
+            if(count($blog) == 0) {
+                $this->error('找不到该博客');
+                return;
+            }
+            $blog = $blog[0];
+            $this->assign('blog', $blog);
+            $this->display('Blog/modifyBlog');
+        } else {
+            redirect('/User/User/user');
+        }
+    }
+
+    public function modifyBlogFormAction() {
+        $blog = D('Blog');
+        $where['user_id'] = I('post.user_id');
+        $where['blog_id'] = I('post.blog_id');
+        $data['title'] = I('post.title');
+        $data['content'] = I('post.content');
+        $data['last_modify_time'] = date('Y-m-d H:i:s');
+        $blog->where($where)->save($data);
+        redirect('/User/User/user');
+    }
+
+    public function deleteBlogAction($userId, $blogId) {
+        if(session('?userId') != $userId) {
+            $this->error('无法删除别人的博客');
+            return;
+        }
+        if(session('?userId') && session('?userToken')) {
+            $blog = D('Blog');
+            $data['user_id'] = $userId;
+            $data['blog_id'] = $blogId;
+            $blog->where($data)->delete();
+        }
+        redirect('/User/User/user');
+    }
+
+    public function detailBlogAction($user,$blogId) {
+        $tempUser = D("User");
+        $result = $tempUser->getByName($user);
+        if(count($result) == 0) {
+            $this->error('找不到用户:' .$user);
+            return;
+        }
+        $userId = $result['id'];
+        $blog = D('Blog');
+        $data['user_id'] = $userId;
+        $data['blog_id'] = $blogId;
+        $blog = $blog->where($data)->select();
+        if(count($blog) == 0) {
+            $this->error('找不到该博客');
+            return;
+        }
+        $blog = $blog[0];
+        $blog['username'] = $user;
+        $this->assign('blog', $blog);
+        $this->assign('userId', $userId);
+        $this->display('Blog/detailBlog');
+    }
+
+}
--- Blog_Web/User/Controller/HomeController.class.php
+++ Blog_Web/User/Controller/HomeController.class.php
@@ -12,6 +12,10 @@ use Think\Controller;
 
 class HomeController extends Controller {
 
+    public function homeAction() {
+        $this->display('Home/home');
+    }
+
     public function userAction($username) {
         $user = D("User");
         $result = $user->getByName($username);
@@ -26,104 +30,6 @@ class HomeController extends Controller {
             $this->assign('userId', $userId);
             $this->display('Home/user');
         }
-    }
-
-    public function addBlogAction() {
-        if(session('?userId') && session('?userToken')) {
-            $this->display('Home/addBlog');
-        } else {
-            redirect('/User/User/user');
-        }
-    }
-
-    public function addBlogFormAction() {
-        $blog = D('Blog');
-        $data['user_id'] = session('userId');
-        $blog = $blog->where($data)->order('blog_id desc')->select();
-        $blog_id = $blog[0]['blog_id']+1;
-        $blog = D('Blog');
-        $data['title'] = I('post.title');
-        $data['user_id'] = session('userId');
-        $data['blog_id'] = $blog_id;
-        $data['content'] = I('post.content');
-        $data['create_time'] = date('Y-m-d H:i:s');
-        $data['last_modify_time'] = date('Y-m-d H:i:s');
-        if(!$blog->create($data)) {
-            $this->error(structureErrorInfo($blog->getError()));
-        } else {
-            $blog->add();
-            redirect('/User/User/user');
-        }
-    }
-
-    public function modifyBlogAction($userId, $blogId) {
-        if(session('?userId') != $userId) {
-            $this->error('无法修改别人的博客');
-            return;
-        }
-        if(session('?userId') && session('?userToken')) {
-            $blog = D('Blog');
-            $data['user_id'] = $userId;
-            $data['blog_id'] = $blogId;
-            $blog = $blog->where($data)->select();
-            if(count($blog) == 0) {
-                $this->error('找不到该博客');
-                return;
-            }
-            $blog = $blog[0];
-            $this->assign('blog', $blog);
-            $this->display('Home/modifyBlog');
-        } else {
-            redirect('/User/User/user');
-        }
-    }
-
-    public function modifyBlogFormAction() {
-        $blog = D('Blog');
-        $where['user_id'] = I('post.user_id');
-        $where['blog_id'] = I('post.blog_id');
-        $data['title'] = I('post.title');
-        $data['content'] = I('post.content');
-        $data['last_modify_time'] = date('Y-m-d H:i:s');
-        $blog->where($where)->save($data);
-        redirect('/User/User/user');
-    }
-
-    public function deleteBlogAction($userId, $blogId) {
-        if(session('?userId') != $userId) {
-            $this->error('无法删除别人的博客');
-            return;
-        }
-        if(session('?userId') && session('?userToken')) {
-            $blog = D('Blog');
-            $data['user_id'] = $userId;
-            $data['blog_id'] = $blogId;
-            $blog->where($data)->delete();
-        }
-        redirect('/User/User/user');
-    }
-
-    public function detailBlogAction($user,$blogId) {
-        $tempUser = D("User");
-        $result = $tempUser->getByName($user);
-        if(count($result) == 0) {
-            $this->error('找不到用户:' .$user);
-            return;
-        }
-        $userId = $result['id'];
-        $blog = D('Blog');
-        $data['user_id'] = $userId;
-        $data['blog_id'] = $blogId;
-        $blog = $blog->where($data)->select();
-        if(count($blog) == 0) {
-            $this->error('找不到该博客');
-            return;
-        }
-        $blog = $blog[0];
-        $blog['username'] = $user;
-        $this->assign('blog', $blog);
-        $this->assign('userId', $userId);
-        $this->display('Home/detailBlog');
     }
 
 }
