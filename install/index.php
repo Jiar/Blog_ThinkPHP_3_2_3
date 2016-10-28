3317ee5503bad5065b6fe2b846d8b4e5a54e7467
Jiar <jiar.world@gmail.com>
2016-10-28 10:59:48 +0800

    modify project install file

--- install/index.php
+++ install/index.php
@@ -7,19 +7,82 @@ define('KOL_ROOT', substr(dirname(__FILE__), 0, -strlen('install')));
 
 include_once('header.php');
 $step = $_GET['step'];
-if($step == 3)  {
+if($step == 4)  {
 	$status = 1;
-	$result = exec(' cd '.KOL_ROOT.' && /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/develop',$output,$status);
+	echo 'KOL_ROOT : ' .KOL_ROOT;
+	echo '<br/><br/>';
 
-	echo '$result : ' .$result .'<br/><br/>';
-	echo '$status : ' .$status .'<br/><br/>';
-	echo '$output : ' .$output .'<br/><br/>';
+	// step 1
+	$result = exec('cd '.KOL_ROOT,$output,$status);
 
+	echo 'step 1';
+	echo '<br/><br/>';
+
+	echo '$result : ';
+	var_dump($result);
+	echo '<br/><br/>';
+
+	echo '$status : ';
+	var_dump($status);
+	echo '<br/><br/>';
+
+	echo '$output : ';
+	var_dump($output);
+	echo '<br/><br/>';
+
+	// step 2
+	$result = exec('/usr/bin/git fetch --all',$output,$status);
+
+	echo 'step 2';
+	echo '<br/><br/>';
+
+	echo '$result : ';
+	var_dump($result);
+	echo '<br/><br/>';
+
+	echo '$status : ';
+	var_dump($status);
+	echo '<br/><br/>';
+
+	echo '$output : ';
+	var_dump($output);
+	echo '<br/><br/>';
+
+	// step 3
+	$result = exec('/usr/bin/git reset --hard origin/develop',$output,$status);
+
+	echo 'step 3';
+	echo '<br/><br/>';
+
+	echo '$result : ';
+	var_dump($result);
+	echo '<br/><br/>';
+
+	echo '$status : ';
+	var_dump($status);
+	echo '<br/><br/>';
+
+	echo '$output : ';
+	var_dump($output);
+	echo '<br/><br/>';
+
+	// step 4
 	$result = exec('rm -r Blog_Web/Runtime',$output,$status);
 
-	echo '$result : ' .$result .'<br/><br/>';
-	echo '$status : ' .$status .'<br/><br/>';
-	echo '$output : ' .$output .'<br/><br/>';
+	echo 'step 4';
+	echo '<br/><br/>';
+
+	echo '$result : ';
+	var_dump($result);
+	echo '<br/><br/>';
+
+	echo '$status : ';
+	var_dump($status);
+	echo '<br/><br/>';
+
+	echo '$output : ';
+	var_dump($output);
+	echo '<br/><br/>';
 
 	if ($status == 0){
 	     echo '<script type="text/javascript"> alert("发布成功") </script>';
@@ -37,7 +100,7 @@ $PHP_SELF = addslashes(htmlspecialchars($_SERVER['PHP_SELF'] ? $_SERVER['PHP_SEL
 ?>
 
 <body>
-	<form method="post" action="index.php?step=3">
+	<form method="post" action="index.php?step=4">
 		<div class="form-actions">
 			<button type="submit" class="pull-right btn btn-primary">一键发布（测试环境）</button>
 		</div>
